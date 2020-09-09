173e7b8d3bb7682a9614ddec28ca0fe1fe08c00d
Cesar Valle <cevpino@gmail.com>
2020-09-09 01:37:55 -0600

    Primer Commit del proyecto

---
+++ src/test/java/com/cevp/market/MarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cevp.market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
