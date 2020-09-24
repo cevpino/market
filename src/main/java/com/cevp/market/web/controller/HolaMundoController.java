4726c58246bd5ac9275c20534ba833a8221e20bd
Cesar Valle <cevpino@gmail.com>
2020-09-23 23:20:41 -0600

    Aqui se agregan archivos de prueba html y css

---
+++ src/main/java/com/cevp/market/web/controller/HolaMundoController.java
@@ -0,0 +1,15 @@
+package com.cevp.market.web.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RestController
+@RequestMapping("/saludar")
+public class HolaMundoController {
+
+    @GetMapping("/hola")
+    public String saludar(){
+        return "Nunca pares de aprender";
+    }
+}
