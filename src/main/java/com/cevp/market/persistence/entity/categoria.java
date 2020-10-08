c9bdae76cee547f50704eebe760e258d2e4d5fd2
Cesar Valle <cevpino@gmail.com>
2020-10-08 07:21:39 -0600

    Se incluye clase categoria

---
+++ src/main/java/com/cevp/market/persistence/entity/categoria.java
@@ -0,0 +1,54 @@
+package com.cevp.market.persistence.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "categorias")
+public class categoria {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_categoria")
+    private Integer idCategoria;
+
+    @Column(name = "descripcion")
+    private String descripcion;
+
+    @Column(name = "estado")
+    private Boolean estado;
+
+
+    public Integer getIdCategoria() {
+        return this.idCategoria;
+    }
+
+    public void setIdCategoria(Integer idCategoria) {
+        this.idCategoria = idCategoria;
+    }
+
+    public String getDescripcion() {
+        return this.descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public Boolean isEstado() {
+        return this.estado;
+    }
+
+    public Boolean getEstado() {
+        return this.estado;
+    }
+
+    public void setEstado(Boolean estado) {
+        this.estado = estado;
+    }
+    
+}
