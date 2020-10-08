5ae03466c70ec56265c76d0f3c875b5fc176cc17
Cesar Valle <cevpino@gmail.com>
2020-10-08 07:14:41 -0600

    Incluye clase Compra

---
+++ src/main/java/com/cevp/market/persistence/entity/compra.java
@@ -0,0 +1,83 @@
+package com.cevp.market.persistence.entity;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "compras")
+public class compra {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_compra")
+    private Integer idCompra;
+
+    @Column(name = "id_cliente")
+    private String idCliente;
+
+    @Column(name = "fecha")
+    private LocalDateTime fecha;
+
+    @Column(name = "medio_pago")
+    private String medioPago;
+
+    @Column(name = "comentario")
+    private String comentario;
+
+    @Column(name = "estado")
+    private String estado;
+
+    public Integer getIdCompra(){
+        return idCompra;
+    }
+
+    public void setIdCompra(Integer idCompra){
+        this.idCompra = idCompra;
+    }
+
+    public String getIdCliente(){
+        return idCliente;
+    }
+
+    public void setIdCliente(String idCliente){
+        this.idCliente = idCliente;
+    }
+
+    public LocalDateTime getFecha(){
+        return fecha;
+    }
+
+    public void setFecha(LocalDateTime fecha){
+        this.fecha = fecha;
+    }
+
+    public String getMedioPago(){
+        return medioPago;
+    }
+
+    public void setMedioPago(String medioPago){
+        this.medioPago = medioPago;
+    }
+
+    public String getComentario(){
+        return comentario;
+    }
+
+    public void setComentario(String comentario){
+        this.comentario = comentario;
+    }
+
+    public String getEstado(){
+        return estado;
+    }
+
+    public void setEstado(String estado){
+        this.estado = estado;
+    }
+}
---
+++ src/main/java/com/cevp/market/persistence/entity/producto.java
@@ -0,0 +1,93 @@
+package com.cevp.market.persistence.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "productos")
+public class producto {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_producto")
+    private Integer idProducto;
+
+    @Column(name = "nombre")
+    private String nombre;
+
+    @Column(name = "id_categoria")
+    private Integer idCategoria;
+
+    @Column(name = "codigo_barras")
+    private String codigoBarras;
+
+    @Column(name = "precio_venta")
+    private Double precioVenta;
+
+    @Column(name = "cantidad_stock")
+    private Integer cantidadStock;
+
+    @Column(name = "estado")
+    private Boolean estado;
+
+
+    public Integer getIdProducto(){
+        return idProducto;
+    }
+
+    public void setIdProducto(Integer idProducto){
+        this.idProducto = idProducto;
+    }
+
+    public String getNombre(){
+        return nombre;
+    }
+
+    public void setNombre(String nombre){
+        this.nombre = nombre;
+    }
+
+    public Integer getIdCategoria(){
+        return idCategoria;
+    }
+
+    public void setIdCategoria(Integer idCategoria){
+        this.idCategoria = idCategoria;
+    }
+
+    public String getCodigoBarras(){
+        return codigoBarras;
+    }
+
+    public void setCodigoBarras(String codigoBarras){
+        this.codigoBarras = codigoBarras;
+    }
+
+    public Double getPrecioVenta(){
+        return precioVenta;
+    }
+
+    public void setPrecioVenta(Double precioVenta){
+        this.precioVenta = precioVenta;
+    }
+
+    public Integer getCantidadStock(){
+        return cantidadStock;
+    }
+
+    public void setCantidadStock(Integer cantidadStock){
+        this.cantidadStock = cantidadStock;
+    }
+
+    public boolean getEstado(){
+        return estado;
+    }
+
+    public void setEstado(boolean estado){
+        this.estado = estado;
+    }
+}
